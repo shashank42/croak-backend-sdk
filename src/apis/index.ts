2a5b3ec1adfa2adcf83727c7fd126d279075d877
shashank42 <devo.shanky@gmail.com>
2022-07-29 03:55:41 +0530

    Codegen openapi

---
+++ src/apis/index.ts
@@ -0,0 +1,14 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './AdminAPIsApi';
+export * from './AirdropAPIsApi';
+export * from './ContractApisApi';
+export * from './CustodianWalletAPIsDeprApi';
+export * from './EmailAuthApi';
+export * from './MarketApisApi';
+export * from './NonCustodialWalletAPIsApi';
+export * from './TokenDataApisApi';
+export * from './TokenDataGroupApisApi';
+export * from './TokenTransferAPIsApi';
+export * from './WalletAuthAPIsApi';
+export * from './WalletBalanceAPIsApi';
